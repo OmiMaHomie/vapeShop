83c3ada7c9c91ba61bf53e12f2eb7502804c321b
Om Khadka <om.khadka.101@icloud.com>
2021-06-01 12:34:33 -0400

    working on media queries for main page.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //Code initially starts of with only the popup.
 let button = document.getElementById('popup-card');
 let mainText = document.getElementById('main-text');
-// mainText.style.visibility = 'hidden';
+//mainText.style.visibility = 'hidden';
 
 //When button1 is pressed, buttonValue becomes true.
 function showPage() {
@@ -14,12 +14,4 @@ function exit () {
     window.location.href="http://www.google.com";
 }
 
-//In the other-showcase-body, the text will have the same width as the picture
-$(document).ready(function() {
-    $("#otherShowcaseFig").css({
-      'width': ($("#other-showcase-body img").width() + 'px')
-    });
-  });
-
-  $(window).resize(function(){ otherShowcaseFig(); });
 
