6530e34b61a749b429bee81a32c7bb127aca4abf
Om Khadka <om.khadka.101@icloud.com>
2021-06-26 15:38:13 -0400

    final draft

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 //Vars
-let button = document.getElementById('popup-card');
-let mainText = document.getElementById('main-text');
-let locationBtn = document.getElementById('locationBtn');
-let locationCard = document.getElementById('location-card');
+var button = document.getElementById('popup-card');
+var mainText = document.getElementById('main-text');
+var locationBtn = document.getElementById('locationBtn');
+var locationCard = document.getElementById('location-card');
+var contactBtn = document.getElementById('contactBtn');
+let card = document.getElementById('contact-card');
 locationCard.style.visibility = 'hidden';
+card.style.visibility = 'hidden';
 mainText.style.visibility = 'hidden';
 
 //When button1 is pressed, buttonValue becomes true.
 function showPage() {
-    mainText.style.visibility = 'visible';
     button.style.visibility = 'hidden';
+    mainText.style.visibility = 'visible';
 }
 
 //When button2 is pressed, send user back to google.
@@ -19,12 +22,22 @@ function exit () {
 
 //When location button is pressed, show location card.
 function showCard() {
-    locationCard.style.visibility = 'visible';
     mainText.style.visibility = 'hidden';
+    locationCard.style.visibility = 'visible';
 }
 
 function closeCard() {
     locationCard.style.visibility = 'hidden';
+    mainText.style.visibility = 'visible';
+}
+
+function contactCard () {
+    mainText.style.visibility = 'hidden';
+    card.style.visibility = 'visible';
+}
+
+function closeContactCard () {
+    card.style.visibility = 'hidden';
     mainText.style.visibility = 'visible';
 }
 
