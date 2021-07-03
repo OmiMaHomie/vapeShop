0ab18a7627d31b7e94754dfed68e6a43fab64db7
Om Khadka <om.khadka.101@icloud.com>
2021-07-03 11:50:15 -0400

    added navbar and slideshow

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-//initial vars and settings on page load
+//Page display controls
+//initial vars and settings
 var agePopup = document.getElementById("age-popup");
 var mainText = document.getElementById("main");
-agePopup.style.visibility = 'visible';
-mainText.style.visibility = 'hidden';
+// agePopup.style.display = 'initial';
+// mainText.style.display = 'none';
 
 //makes the user leave the page to google
 function leaveSite () {
@@ -11,6 +12,32 @@ function leaveSite () {
 
 //shows main page and hides age popup
 function showPage () {
-    agePopup.style.visibility = 'hidden';
-    mainText.style.visibility = 'visible';
+    agePopup.style.display = 'none';
+    mainText.style.display = 'initial';
+}
+
+
+//Slider controls
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
 }
